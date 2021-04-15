284b923df690fdfbc278edeea6664cfb3f779cdb
garcila <germanarcila@gmail.com>
2021-04-14 20:13:07 -0400

    super late first commit

---
+++ pages/posts/git-notes.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/layout';
+
+export default function GitNotes(props) {
+  return (
+    <Layout>
+      <Head>
+        <title>Git Notes</title>
+      </Head>
+      <h1>Git Notes</h1>
+    </Layout>
+  );
+}
